4e1346b7ca5e76e31189ebd29c9f63ed6e35c063
Mark Mahoney <bcmark@gmail.com>
2018-03-04 16:47:57 -0800

    clean up bugs I found installing on a new machine

--- init.el
+++ init.el
@@ -1,8 +1,11 @@
+
 ;; Added by Package.el.  This must come before configurations of
 ;; installed packages.  Don't delete this line.  If you don't want it,
 ;; just comment it out by adding a semicolon to the start of the line.
 ;; You may delete these explanatory comments.
 (package-initialize)
+
+(add-to-list 'load-path "/Users/markmahoney/.emacs.d/init")
 
 (load "package-installation")
 
@@ -14,13 +17,11 @@
  'web-mode
  'js2-mode
  'less-css-mode
- 'scala-mode2)
+ 'scala-mode)
 
 (if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
 (if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
 (if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
-
-(add-to-list 'load-path "/Users/markmahoney/.emacs.d/init")
 
 ;; Disable backup
 (setq make-backup-files nil)
@@ -55,3 +56,18 @@
 (load "modes")
 (load "key-bindings")
 (load "indentation")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(js2-basic-offset 2)
+ '(package-selected-packages
+   (quote
+    (scala-mode yaml-mode web-mode scala-mode2 paredit less-css-mode js2-mode flymake-ruby erlang color-theme-modern clojure-mode))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
